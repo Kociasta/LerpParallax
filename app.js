96735a5ee43673489d59f64f99b26da462b2950d
Katarzyna Pieniazek <pieniazek.k@gmail.com>
2017-08-12 18:30:04 +0200

    set things for elem1

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ $(()=>{
   var scene = $(".scene");
   var elements = $(".element");
 
-  var square1 = elements[0];
+  var elem1 = elements[0];
 
   // get data from html
-  var x = square1.dataset.x;
-  var y = square1.dataset.y;
-  var z = square1.dataset.z;
-  var speed = square1.dataset.speed;
+  var x = elem1.dataset.x;
+  var y = elem1.dataset.y;
+  var z = elem1.dataset.z;
+  var speed = elem1.dataset.speed;
 
-  // set basic position of square1
-  $(square1).css("background-color" , "rgb(185, 5, 90)");
-  $(square1).css("left" , x+"px");
-  $(square1).css("top" , y+"px");
+  // set basic position of elem1
+  $(elem1).css("background-color" , "rgb(185, 5, 90)");
+  $(elem1).css("left" , x+"px");
+  $(elem1).css("top" , y+"px");
 
   // set event on mousemove
   scene.on("mousemove" , onMousemove);
