b15756027e0576937f9113ce472860b2e76cb130
Katarzyna Pieniazek <pieniazek.k@gmail.com>
2017-08-12 23:22:03 +0200

    woooooo so many things! parallax is done

--- app.js
+++ app.js
@@ -4,43 +4,52 @@ $(()=>{
   var currentMousePositionX=0 ;
   var currentMousePositionY=0 ;
 
-  var elem1 = elements[0];
-
-  // get data from html
-  var x = elem1.dataset.x;
-  var y = elem1.dataset.y;
-  var z = elem1.dataset.z;
-  var speed = elem1.dataset.speed;
-
-  // set basic position of elem1
-  $(elem1).css("background-color" , "rgb(185, 5, 90)");
-  $(elem1).css("left" , x+"px");
-  $(elem1).css("top" , y+"px");
-
-  // set event on mousemove
+    // set event on mousemove
   scene.on("mousemove" , onMousemove);
 
   // ------------------functions---------------------
 
   function onMousemove(event){
+    // typeof event.clientX/Y  is number
     currentMousePositionX = event.clientX;
     currentMousePositionY = event.clientY;
   }
-
+  // linear interpolation
   function lerp(oldPosition, newPosition, t){
     return newPosition*t+(1-t)*oldPosition;
   };
-
+  // it ensure that calculation will be done after mouse event end
   function loop(){
+    // every element is calculated separately
+    $(elements).each((i , elem)=>{
+      let orgX = parseInt(elem.dataset.x , 10);
+      let orgY = parseInt(elem.dataset.y , 10);
 
-    x = currentMousePositionX;
-    y = currentMousePositionY;
+      let dX = currentMousePositionX - orgX;
+      let dY = currentMousePositionY - orgY;
 
-    $(elem1).css("left" , x+"px");
-    $(elem1).css("top" , y+"px");
+      let speed = parseFloat(elem.dataset.speed) ;
+      let dX2= dX*speed;
+      let dY2= dY*speed;
 
-    // console.log(x);
+      let targetX = orgX + dX2;
+      let targetY = orgY + dY2;
+
+      let currX = parseInt($(elem).css("left"));
+      let currY = parseInt($(elem).css("top"));
+
+      let newX = lerp(currX , targetX , 0.1);
+      let newY = lerp(currY , targetY , 0.1);
+
+
+      $(elem).css("left" , newX+"px");
+      $(elem).css("top" , newY+"px");
+
+    });
+
+
     requestAnimationFrame(loop);
+
 
   }
 
